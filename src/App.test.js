a9799dcdee171585c3167c5cfab75d638dcbe06a
Linden Chiu <hchiu87@gmail.com>
2019-12-17 22:50:50 -0800

    setup initial component/test files

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders todo list component', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const list = getByText(/hello/i);
+  expect(list).toBeInTheDocument();
 });
---
+++ src/ToDoList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ToDoList = () => {
+  return <div> Hello World </div>
+}
+
+export default ToDoList;
